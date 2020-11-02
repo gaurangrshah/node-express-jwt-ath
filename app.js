ec94fb705c414e8490264cd60f5202d093be499a
gaurangrshah <gaurang.r.shah@gmail.com>
2020-11-01 19:52:12 -0500

    example: cookie persistence

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-// middleware -- allows serving of static files such as images/css
-app.use(express.static("public"));
+// middleware
+app.use(express.static("public")); // allows serving of static files such as images/css
 app.use(express.json()); // will run on each route that applies (DEL/PUT/POST requests)
+app.use(cookieParser()); // library to help manage cookies
 
 // view engine -- uses ejs as a view engine
 app.set("view engine", "ejs");
@@ -27,3 +29,26 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies")); // controller
 app.use(authRoutes);
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  // NOTE: cookie value is a key/value pair: {newUser: true}
+  // ❌ res.setHeader("Set-Cookie", "newUser=true");
+  res.cookie("newUser", false);
+  // 3rd argument is an options object
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+  // cookie expiry = maxAge = 1 day (24 hrs)
+  // 1000 milliseconds * 60 seconds * 60 min * 24 hrs
+
+  // cookies will get sent back with the response's headers and be accessible from the browser
+  res.send("you got cookie");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies.newUser);
+});
